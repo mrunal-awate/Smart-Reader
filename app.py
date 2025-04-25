f7c74e61df33e064a66d32e99b35d91b8d9457c8
mrunalawate <mrunalawate0tv@gmail.com>
2025-04-26 03:31:08 +0530

    Update app.py to run on 0.0.0.0 and port 5000

--- app.py
+++ app.py
@@ -86,4 +86,4 @@ def serve_audio(filename):
     return send_from_directory(app.config['OUTPUT_FOLDER'], filename)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
